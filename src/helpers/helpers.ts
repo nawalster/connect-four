734394955b5df72cd2a6e31e67da41c497cfee73
nawalster <nawal.alsaidi@gmail.com>
2023-05-24 15:11:01 -0400

    extract game board functions to a helper file

--- src/helpers/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const clearBoardDOM = () => {
-  [...document.querySelectorAll(".game-piece")].forEach((piece) => {
-    piece.remove();
-  });
-  [...document.querySelectorAll(".piece-win-marker")].forEach((marker) => {
-    marker.remove();
-  });
-};
